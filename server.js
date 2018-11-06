e1eb9e3f0c3dff703a105d6b84035b91a4f683a2
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-06 08:24:43 -0800

    rendering checkin and image modules on proxy client

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/listings/:id', express.static(path.join(__dirname, 'client')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
