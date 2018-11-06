af674efa8dbeae84f901531583cbbb7bbae17ed9
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-05 17:20:33 -0800

    set up basic structure of proxy server

---
+++ client/index.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+  React.createElement(Nav),
+  document.getElementById('nav')
+);
+
+// ReactDOM.render(
+//   React.createElement(ImageModule),
+//   document.getElementById('images')
+// );
+
+ReactDOM.render(
+  React.createElement(MainBody),
+  document.getElementById('main-body')
+);
+
